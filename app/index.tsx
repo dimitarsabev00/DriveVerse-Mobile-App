4a843c7cf56bf829b47cebee5293e01d29ffa853
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-11-10 14:13:33 +0200

    Update Root File For Home Screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,11 @@
-import { View, Text } from "react-native";
+import { Text } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const Home = () => {
   return (
-    <SafeAreaView>
-      <Text>Home</Text>
+    <SafeAreaView className="flex-1 justify-center items-center">
+      <Text>DriveVerse Mobile App</Text>
     </SafeAreaView>
   );
 };
