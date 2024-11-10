f12c5fb45114ac2df5c67a9fe5709ab571d93ea6
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-11-10 22:51:39 +0200

    Added Logic For Redirect When Open App, If We Have Authenticate User Redirect To Home Tab Screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,12 @@
-import React from "react";
+import { useAuth } from "@clerk/clerk-expo";
 import { Redirect } from "expo-router";
 
-const Home = () => {
+const Page = () => {
+  const { isSignedIn } = useAuth();
+
+  if (isSignedIn) return <Redirect href="/(root)/(tabs)/home" />;
+
   return <Redirect href="/(auth)/welcome" />;
 };
 
-export default Home;
+export default Page;
