79c6b5d5a2deed02c72d2a73bcd0070bda8980ce
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-11-12 10:19:40 +0200

    Added Ride Interface

--- types/type.d.ts
+++ types/type.d.ts
@@ -19,3 +19,23 @@ declare interface InputFieldProps extends TextInputProps {
   iconStyle?: string;
   className?: string;
 }
+
+declare interface Ride {
+  origin_address: string;
+  destination_address: string;
+  origin_latitude: number;
+  origin_longitude: number;
+  destination_latitude: number;
+  destination_longitude: number;
+  ride_time: number;
+  fare_price: number;
+  payment_status: string;
+  driver_id: number;
+  user_id: string;
+  created_at: string;
+  driver: {
+    first_name: string;
+    last_name: string;
+    car_seats: number;
+  };
+}
